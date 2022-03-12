ce7dfb9396ab5b91fc4126cbef0a1c3eb618f3b4
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-03-11 18:19:01 -0800

    Redesigned format for storing event data through documentation

--- src/eventStorage.js
+++ src/eventStorage.js
@@ -1,7 +1,35 @@
 /**
  * Helper functions for managing localStorage and saving/getting events from it
  */
+
+/**
+ * Prefix used for all localStorage keys related to events
+ * @type {string}
+ */
 const EVENT_KEY_PREFIX = "EVENT-DATA";
+
+/**
+ * Array holding EventData instances for a month. Note that the length of this array can be less than the number of days in the month as days without events are omitted
+ * Not guaranteed to be sorted
+ * @typedef {EventData[]} MonthEventData
+ */
+
+/**
+ * Contains all the event data for a given day
+ * Storage format is an object that uses the day of the month as the key
+ * @typedef EventData
+ * @property {number} day Day of the month (1-31)
+ * @property {EventEntry[]} events Array of events for the given day
+ */
+
+/**
+ * Object that contains the information about a single event
+ * @typedef EventEntry
+ * @property {string} title Label for an event
+ * @property {string} [description=""] Description for an event
+ * @property {string} start Date string for the start of an event
+ * @property {string} end Date string for the end of an event
+ */
 
 /**
  * Checks the ranges of months and years and throws errors when out of range
@@ -20,17 +48,27 @@ function rangeCheck(month, year) {
 }
 
 /**
+ * Converts a month and year to a localStorage key
+ * Warning: Does not validate
+ * @param {number} month Month number from 1-12
+ * @param {number} year Full year number
+ * @return {string} String to use as a key for localStorage
+ */
+function toKey(month, year) {
+	return `${EVENT_KEY_PREFIX}-${month}-${year}`;
+}
+
+/**
  * Fetches all events from a specified month
  * @param {number} month Month number from 1-12
  * @param {number} year Full year number
  * @throws {RangeError} Thrown when month is not between 1-12
  * @throws {RangeError} Thrown when year is not a number greater than or equal to 0
- * @return {Object} Returns an object where each day from 1-31 is an array of events or nonexistent
+ * @return {MonthEventData} Returns an entire month's worth of events
  */
 export function getMonthEvents(month, year) {
 	rangeCheck(month, year);
-	const data = {};
-	return data;
+	return JSON.parse(localStorage.getItem(toKey(month, year))) || {days: [], length: 0};
 }
 
 /**
@@ -43,4 +81,5 @@ export function getMonthEvents(month, year) {
  */
 export function serialize(month, year, events) {
 	rangeCheck(month, year);
+	localStorage.setItem(toKey(month,year), JSON.stringify(events));
 }
